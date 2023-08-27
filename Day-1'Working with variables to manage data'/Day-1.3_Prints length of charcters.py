ece00905568ccaea321d76be2dae93fe2082afb3
Aakki-K <akashkulli305@gmail.com>
2023-08-27 19:42:49 +0530

    started

---
+++ Day-1'Working with variables to manage data'/Day-1.3_Prints length of charcters.py
@@ -0,0 +1,33 @@
+'''Write a program that prints the number of characters in a user's name'''
+'''
+Input: Akash
+Output: 5
+'''
+# TODO 1: Code
+
+Name = input("Enter your name : ")
+print(len(Name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
