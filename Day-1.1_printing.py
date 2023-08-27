677105ba140620652c1c5d2eb28e3300934a69c4
Aakki-K <akashkulli305@gmail.com>
2023-08-27 14:49:37 +0530

    Start

---
+++ Day-1.1_printing.py
@@ -0,0 +1,22 @@
+"""After you have written your code,
+you should run your program, and it should print the following:
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+# TODO 1: MyCode
+
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day-1.2_Debugging.py
@@ -0,0 +1,19 @@
+# Debugging Exercise
+'''Output:
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.'''
+
+# Code:
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
+
+
+
+
