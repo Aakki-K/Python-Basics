01c4bb8865f5d9393044041a01f061281264f243
Aakki-K <akashkulli305@gmail.com>
2023-08-30 23:12:42 +0530

    Day2 Start

---
+++ Day-2 'Data types & string manupulation'/Day-2.1_39 = 3+9 = 12.py
@@ -0,0 +1,7 @@
+'''Write a program that adds the digits in a two digit number. e.g. if the input was 35,
+ then the output should be 3 + 5 = 8'''
+
+Number = input("Enter 2 digit Number : ")
+Sum = int(Number[0]) + int(Number[1])
+print(Sum)
+
