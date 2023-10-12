36f6081dd8715dfbafbf02f7229bd0839e42e98d
Aakki-K <akashkulli305@gmail.com>
2023-10-12 22:15:22 +0530

    9.1

---
+++ Day-9_Dictonary with list & nest/Day-9.1_Grading Program.py
@@ -0,0 +1,66 @@
+# TODO 1: Task
+"""
+Write a program that converts their scores to grades.
+By the end of your program, you should have a new dictionary called
+student_grades that should contain student names for keys
+and their grades for values.
+
+This is the scoring criteria:
+Scores 91 - 100: Grade = "Outstanding"
+Scores 81 - 90: Grade = "Exceeds Expectations"
+Scores 71 - 80: Grade = "Acceptable"
+Scores 70 or lower: Grade = "Fail"
+"""
+
+# TODO 2: challenge
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    if student_scores[student] > 90:
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
